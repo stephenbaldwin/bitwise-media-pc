ced068577e7e1fef5ed18fca7c834e47201ceffc
Stephen <stephenbaldwin@me.com>
2013-03-23 21:19:03 -0400

    'tests'

---
+++ proxy.rb
@@ -0,0 +1,14 @@
+require 'em-proxy'
+require 'faraday'
+
+Proxy.start(:host => "0.0.0.0", :port => 8080, :debug => false) do |conn|
+
+  conn.on_data do |data|
+    http = Faraday.new(:url => 'http://localhost:9292') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+    http.get "/#{data}"
+  end
+end
